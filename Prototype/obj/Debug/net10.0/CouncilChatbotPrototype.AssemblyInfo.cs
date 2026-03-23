0f143a95a73f7db96bdf1e1e95e9f337bfd3c51b
samikkhan <77587298+samikkj@users.noreply.github.com>
2026-03-23 15:42:58 +0000

    dockerfile
    Dockerfile

--- Prototype/obj/Debug/net10.0/CouncilChatbotPrototype.AssemblyInfo.cs
+++ Prototype/obj/Debug/net10.0/CouncilChatbotPrototype.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CouncilChatbotPrototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99754e1d2fb96312fcd2e9decd08771239e0274b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd6dca75f9e824f095a0be6ed2c2b013fb0fca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CouncilChatbotPrototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CouncilChatbotPrototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
